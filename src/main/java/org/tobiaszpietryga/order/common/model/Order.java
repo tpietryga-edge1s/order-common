715fd98d7fa83c3db56d4f7ea031b115bf2d2706
Tobiasz Pietryga <tobiasz.pietryga@eo.pl>
2025-04-02 16:04:46 +0200

    poprawki

--- src/main/java/org/tobiaszpietryga/order/common/model/Order.java
+++ src/main/java/org/tobiaszpietryga/order/common/model/Order.java
@@ -1,5 +1,6 @@
 package org.tobiaszpietryga.order.common.model;
 
+import java.beans.ConstructorProperties;
 import java.math.BigDecimal;
 
 import lombok.Builder;
@@ -10,6 +11,17 @@ import lombok.Value;
 @Builder
 @Data
 public class Order {
+	@ConstructorProperties({"id", "customerId", "name", "status", "price", "paymentStarted", "stockStarted"})
+	public Order(Long id, Long customerId, String name, Status status, Integer price, boolean paymentStarted, boolean stockStarted) {
+		this.id = id;
+		this.customerId = customerId;
+		this.name = name;
+		this.status = status;
+		this.price = price;
+		this.paymentStarted = paymentStarted;
+		this.stockStarted = stockStarted;
+	}
+
 	Long id;
 	Long customerId;
 	String name;
